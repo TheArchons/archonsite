23b018c428d8649b8cc593c509f9b71b918f022e
thearchons <github@thearchons.xyz>
2025-05-17 10:44:47 -0700

    header

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,48 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import pfp from "../static/img/pfp.webp";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Archonssite" },
+    { name: "description", content: "Archons' Site!" },
   ];
 }
 
+function Header() {
+  return <div className="header">
+    <a href="/" className="header-title header-item">Archons</a>
+
+    <a href="#about" className="header-item">About</a>
+    <a href="#projects" className="header-item">Projects</a>
+    <a href="#contact" className="header-item">Contact</a>
+  </div>
+}
+
+function Hero() {
+  return (
+    <div className="hero section" id="about">
+      <div>
+        <h1>Archons</h1>
+        <h2>Full Stack Developer</h2>
+        <p>
+        Hi! I'm Alexander Li, an incoming Computer Science National Scholar at the University of Toronto.<br/><br/>
+        I am interested in a variety of subjects, but at the moment I am mainly exploring:<br/>
+        🌐 Front-end: Web Development: ReactJS, as well as HTML, CSS, and JS.<br/>
+        💾 Back-end: NodeJS, Django, REST APIs, SQL.<br/>
+        🔒 Cybersecurity: Reverse Engineering, Steganography<br/>
+        </p>
+        <div style={{ marginTop: "1em" }}><a className="btn btn-large" href="/resume">Resume</a></div>
+      </div>
+      <img className="pfp" src={pfp} />
+      
+    </div>
+  )
+}
+
 export default function Home() {
-  return <Welcome />;
+  return (<div className="flex flex-col items-center">
+    <Header />
+    <Hero />
+  </div>)
 }
